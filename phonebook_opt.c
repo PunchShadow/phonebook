fe5a02b3af66a75cb06a1647019ee0a6c441a150
PunchShadow <littleuniverse24@gmail.com>
2018-03-09 22:24:02 +0800

    Use the origin function of orig.c in opt.c

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,16 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
-/* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
+/*  FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    /* implement */
+    /* Use the origin */
+    while ( pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
     /* TODO: implement */
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
     return NULL;
 }
